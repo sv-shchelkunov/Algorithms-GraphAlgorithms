3bc58edd02eee055bf5bc599ab428da0809a1549
sv-shchelkunov <sergey.shchelkunov@gmail.com>
2022-10-27 06:48:24 -0400

    small fixes (to have a better notation/readibility) here and there

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -91,21 +91,21 @@ def bellman_ford(nodes, edges, source, show=False):
     path_len[source] = 0
 
     v_1 = len(nodes) - 1
-    for v in range(v_1):
-        for key in edges:
-            for y, _l in edges[key].items():
-                new_path_len = path_len[key] + _l
-                if (new_path_len < path_len[y]):
-                    prev_nodes[y] = key
-                    path_len[y] = new_path_len
+    for _ in range(v_1):
+        for _v in edges:
+            for _u, _l in edges[_v].items():
+                new_path_len = path_len[_v] + _l
+                if (new_path_len < path_len[_u]):
+                    prev_nodes[_u] = _v
+                    path_len[_u] = new_path_len
 
-    for v in range(v_1):
-        for key in edges:
-            for y, _l in edges[key].items():
-                new_path_len = path_len[key] + _l
-                if (new_path_len < path_len[y]):
-                    prev_nodes[y] = key
-                    path_len[y] = -float('inf')
+    for _ in range(v_1):
+        for _v in edges:
+            for _u, _l in edges[_v].items():
+                new_path_len = path_len[_v] + _l
+                if (new_path_len < path_len[_u]):
+                    prev_nodes[_u] = _v
+                    path_len[_u] = -float('inf')
 
     for e_node in nodes:
         _node = e_node
@@ -125,9 +125,10 @@ def bellman_ford(nodes, edges, source, show=False):
     return {'source': source, 'path_way': path_way, 'prev_nodes': prev_nodes, 'path_len': path_len}
 
 """Depth First Search (DFS):
-1. graphs must be bi-directional, and edges must be without any weights;
-2. here, it can find the nodes that are connected;
-3. time complexity of Depth First Search algorithm is O( N + E )
+1. graphs can be bi-directional or not (true? not?) depending on application;
+2. edges must be without any weights (true? not?);
+3. here, it can find the nodes that are connected, but it requires bi-directionality;
+4. time complexity of Depth First Search algorithm is O( N + E )
 usage: DepthFirstSearch(NODES, EDGES).findComponents()
 """
 # since, the method findComponents works correctly only when there are
@@ -176,13 +177,13 @@ class DepthFirstSearch():
                 self.dfs(u)
 
 """Breadth First Search (BFS):
-1. graphs must be bi-directional (undirectional);
+1. graphs must be bi-directional (true? not?);
 2. the edges must be unweighted (weight = 1);
 3. will find optimal path form a given node to another, specified end node;
 4. time complexity of Breadth First Search algorithm is O( N + E )
 usage: BreadthFirstSearch(NODES, EDGES, start_node, end_node).findPath()
 """
-# BFS works correctly only when there is no directions, and edges have no weigth;
+# BFS works correctly only when(?) there is no directions, and edges have no weigth;
 # thus, edges are normalized to become bi-directional and have weight = 1
 class BreadthFirstSearch():
     def __init__(self, nodes, edges, source, sink, show=False):
@@ -240,6 +241,18 @@ class BreadthFirstSearch():
             print('end =:', self.sink)
             print('path =:', reconstructed_path)
         return reconstructed_path
+
+"""Topological Sort algorithm (Top Sort):
+1. graphs are directed (I suppose the edge weight = 1 (or the same for every edge));
+2. graphs must have no cycles, that is, be DAG (directed acyclic graphs);
+3. an example is a tree (since by definition, a tree cannot have any cycles);
+4. to detect cycles, one can use Tarjan's strongly connected component algorithm;
+5. topogical orderings are not unique;
+6. time complexity of Topological Sort algorithm is O( N + E )
+"""
+class TopSort():
+    def __init__(self):
+        pass
 
 if __name__ == "__main__":
     dijkstra(NODES, EDGES, 5, show=True)
